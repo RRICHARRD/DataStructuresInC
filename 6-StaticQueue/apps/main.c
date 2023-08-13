6e87dec42e19d0a6cba417a9ee3a16b2166f7dd6
Richard Guilherme <guilherme.rixard@gmail.com>
2023-08-13 14:50:00 -0300

    Created folder structure to learn hoe to implement a Static Queue

---
+++ 6-StaticQueue/apps/main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(){
+
+    printf("Folder struture is syncronized");
+
+    return EXIT_SUCCESS;
+}
---
+++ 6-StaticQueue/src/StaticQueue.c
@@ -0,0 +1,7 @@
+#ifndef STATIC_QUEUE_IMPLEMENTATION
+#define STATIC_QUEUE_IMPLEMENTATION
+
+#include <StaticQueue.h>
+
+
+#endif
