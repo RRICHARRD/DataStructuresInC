1ee6fabf4a3dc93ff49e29bca0eb91a6ee4386fc
Richard Guilherme <guilherme.rixard@gmail.com>
2023-08-17 22:25:21 -0300

    Reading something about tree to understando what is and how to implement, I have builded that in python in the future, now I want to use C to learn more about computer and acquire advanced knowledge

--- 8-Tree/apps/main.c
+++ 8-Tree/apps/main.c
@@ -4,6 +4,17 @@
 
 #include <Tree.h>
 
+/*
+    Reading theory to understand better
+
+    non-linear data structure, before all was linear
+    store data in a hierarchical order
+    each node of a tree has children and parent, the concentual is the same of real trees
+
+    Trees has details, I will learn them in the future to be more productive
+*/
+
+
 int main() {
 
     puts("Program is working!!!");
