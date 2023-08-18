ebcd12571144bedd36c219852c3ffef788bdb169
Richard Guilherme <guilherme.rixard@gmail.com>
2023-08-17 22:08:49 -0300

    Create a structure to review how to create trees

---
+++ 8-Tree/apps/main.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+#include <Tree.h>
+
+int main() {
+
+    puts("Program is working!!!");
+
+    return 0;
+}
---
+++ 8-Tree/src/Tree.c
@@ -0,0 +1,6 @@
+#ifndef STATIC_QUEUE_IMPLEMENTATION
+#define STATIC_QUEUE_IMPLEMENTATION
+
+#include <Tree.h>
+
+#endif
