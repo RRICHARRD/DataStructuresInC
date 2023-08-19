3d6cbcd9eef28b9c79967e72de393c3f9047787e
Richard Guilherme <guilherme.rixard@gmail.com>
2023-08-18 23:16:31 -0300

    Understant what is full binary tree

--- 9-BinaryTree/apps/main.c
+++ 9-BinaryTree/apps/main.c
@@ -3,6 +3,13 @@
 
 #include <BinaryTree.h>
 
+/*
+    A binary tree is a tree in which every node has at most two children. Can ONE but not more then TWO
+
+    types of binary`s trees
+
+        Full Binary Tree → A binary tree in which every node has 2 children except the leaves is known as a full binary tree.
+*/
 
 int main() {
     TreeNode *node = (TreeNode *) create_tree_node(1);
